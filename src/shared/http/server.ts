3485943c5a35e4422f0e31d0a10f59aa07ce5b0c
samuelcpr <samuelcpr464@gmail.com>
2022-08-30 16:58:56 -0300

    chore: integration file it error and server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,25 @@
-import express, { Response, Request } from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 import cors from "cors"
 import routes from "./routes"
+import AppError from '@shared/errors/AppError';
 
 const app = express()
 app.use(cors());
 app.use(express.json())
 app.use(routes)
+
+app.use((error:Error, request:Request, response: Response, next: NextFunction) => {
+if(error instanceof AppError){
+  return response.status(error.statusCode).json({
+    status: "error",
+    message: error.message
+  })
+}
+return response.status(500).json({
+  status: "error",
+  message: "internal server error",
+})
+})
 
 app.listen(3000, () => {
     console.log("🚀🚀✨✨ Servidor on na porta http://localhost:3000/ 💡✨✨ 🚀 ")
