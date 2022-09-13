952844d745c0156029429948a223c5990eb877d4
samuelcpr <samuelcpr464@gmail.com>
2022-09-13 10:02:07 -0300

    feat: creating user deletion services

---
+++ src/modules/users/services/DeleteUserService.ts
@@ -0,0 +1,25 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+
+interface IRequest {
+  id: string;
+
+}
+
+class DeleteUserService {
+   public async execute({id}: IRequest): Promise<void> {
+      const usersRepository = getCustomRepository(UsersRepository);
+
+      const user = await usersRepository.findOne(id);
+
+      if (!user){
+        throw new AppError('product not found.');
+      }
+
+      await usersRepository.remove(user);
+    }
+}
+
+export default DeleteUserService;
+
